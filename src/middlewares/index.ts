a2f0d1ec999c95b40b49a4c8670b1639006ff7dc
k3kys <56282663+k3kys@users.noreply.github.com>
2021-05-31 13:00:14 +0900

    add catchAsync middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,3 +2,4 @@ export * from './src/current-user';
 export * from './src/error-handler';
 export * from './src/require-auth';
 export * from './src/validate-request';
+export * from "./src/catchAsync"
