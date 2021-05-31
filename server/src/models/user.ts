696243a1b6bc1c92a9fb9ae4d9c5b720ef1c98de
k3kys <56282663+k3kys@users.noreply.github.com>
2021-05-31 17:58:54 +0900

    web container update

---
+++ server/src/models/user.ts
@@ -0,0 +1,84 @@
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+
+export interface UserAttrs {
+    name: string
+    email: string
+    password: string
+    confirmPassword: string,
+    university: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc,
+    correctPassword(candidatePassword: string, password: string): Promise<boolean>;
+}
+
+export interface UserDoc extends mongoose.Document {
+    name: string
+    email: string
+    password: string
+    confirmPassword: string
+    isAdmin: boolean,
+    university: string,
+    passwordResetToken: string | undefined,
+}
+
+const userSchema = new mongoose.Schema<UserDoc>({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, "Please confirm your password"],
+        validate: {
+            validator: function (this: UserDoc, el: any) {
+                return el === this.password;
+            },
+            message: "Passwords are not the same!",
+        },
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false 
+    },
+    university: {
+        type: String,
+        required: true
+    },
+    passwordResetToken: String,
+}, {
+    timestamps: true
+})
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs)
+}
+
+userSchema.pre<UserDoc>("save", async function (next) {
+    if (!this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password!, 12)
+
+    next();
+});
+
+userSchema.statics.correctPassword = async function (
+    candidatePassword: string,
+    userPassword: string
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema)
+
+export { User }
