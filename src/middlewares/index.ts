55a912718354be2501eb8fb398e1d6d68bd480c8
k3kys <56282663+k3kys@users.noreply.github.com>
2021-06-01 09:07:43 +0900

    update

--- src/middlewares/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './src/current-user'
-export * from './src/error-handler'
-export * from './src/require-auth'
-export * from './src/validate-request'
-export * from "./src/catchAsync"
-
