6a8328fcb79804e5164c1d08cf92ff09d3c9f655
k3kys <56282663+k3kys@users.noreply.github.com>
2021-06-03 16:56:48 +0900

    profile schema update

---
+++ server/src/models/profile.ts
@@ -0,0 +1,83 @@
+import mongoose from "mongoose"
+
+export interface ProfileAttrs {
+    image: string | undefined
+    motherCountry: string
+    currentCountry: string
+    confirmPassword: string,
+    motherLanguage: [string],
+    learningLanguage: [string],
+    intro: string,
+    social: {
+        facebook: string | undefined,
+        instagram: string | undefined
+    },
+    university: string
+}
+
+interface ProfileModel extends mongoose.Model<ProfileDoc> {
+    build(attrs: ProfileAttrs): ProfileDoc
+}
+
+export interface ProfileDoc extends mongoose.Document {
+    image: string | undefined
+    motherCountry: string
+    currentCountry: string
+    confirmPassword: string,
+    motherLanguage: [string],
+    learningLanguage: [string],
+    intro: string,
+    social: {
+        facebook: string | undefined,
+        twitter: string | undefined,
+        instagram: string | undefined
+    },
+    university: string
+}
+
+const profileSchema = new mongoose.Schema<ProfileDoc>({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+    image: {
+        type: String
+    },
+    motherCountry: {
+        type: String,
+        required: true
+    },
+    currentCountry: {
+        type: String,
+        required: true
+    },
+    motherLanguage: [{
+        type: String,
+        required: true
+    }],
+    learningLanguage: [{
+        type: String,
+        required: true
+    }],
+    intro: {
+        type: String,
+        required: true
+    },
+    social: {
+        facebook: { type: String },
+        instagram: { type: String }
+    },
+    university: {
+        type: String,
+        required: true
+    },
+
+}, {
+    timestamps: true
+})
+
+profileSchema.statics.build = (attrs: ProfileAttrs) => {
+    return new Profile(attrs)
+}
+
+const Profile = mongoose.model<ProfileDoc, ProfileModel>("Profile", profileSchema)
+
+export { Profile }
