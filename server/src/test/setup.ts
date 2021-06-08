49b91ba14d72e35070cc43688a20f590679ea9f6
k3kys <56282663+k3kys@users.noreply.github.com>
2021-06-08 13:10:50 +0900

    profile follower test update

--- server/src/test/setup.ts
+++ server/src/test/setup.ts
@@ -12,6 +12,8 @@ declare global {
     namespace NodeJS {
         interface Global {
             signin(): Promise<string[]>
+            signup(): Promise<any>
+            createProfile(userId: any, university: any): Promise<string[]>
         }
     }
 }
@@ -28,7 +30,8 @@ beforeAll(async () => {
 
     await mongoose.connect(mongoUri, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: true
     });
 });
 
@@ -48,7 +51,9 @@ afterAll(async () => {
     await mongoose.connection.close()
 });
 
-global.signin = async () => {
+global.signup = async () => {
+    jest.setTimeout(300000)
+
     const response = await request(app)
         .post('/api/users/signup')
         .send({
@@ -61,6 +66,52 @@ global.signin = async () => {
         .expect(200);
 
     const cookie = response.get('Set-Cookie')
+    const id = response.body._id
+    const university = response.body.university
+    
+    return { id, university, cookie }
+}
 
+global.signin = async () => {
+    jest.setTimeout(300000)
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            name: "dasd",
+            email: 'test@test.com',
+            password: 'password',
+            confirmPassword: "password",
+            university: "kookmin"
+        })
+        .expect(200);
+
+    const cookie = response.get('Set-Cookie')
+    
     return cookie
+}
+
+global.createProfile = async (userId, university) => {
+    jest.setTimeout(300000)
+
+    const response = await request(app)
+    .post(`/api/profile/createProfile/${userId}`)
+    .send({
+        user: userId,
+        image: "image",
+        motherCountry: "motherCountry",
+        motherLanguage: "motherLanguage",
+        learningLanguage: "learningLanguage",
+        intro: "intro",
+        social: {
+            facebook: "facebook",
+            instagram: "instagram"
+        },
+        university: university
+    })
+    .expect(200)
+
+    const id = response.body._id
+
+    return id
 }
